97a74ff4af438b2aa87255e479e5da4e4867d379
alexdavies440 <alexdavies440@gmail.com>
2024-09-03 23:01:45 -0500

    part D done

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/exercise1.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/exercise1.java
@@ -1,7 +1,7 @@
 package org.launchcode;
 
 public class exercise1 {
-    public static void main() {
-        
+    public static void main(String[] args) {
+        System.out.println("test");
     }
 }
