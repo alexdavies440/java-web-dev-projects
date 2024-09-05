d0153261d0cc653e43882bb140838d2c169ebdc5
alexdavies440 <alexdavies440@gmail.com>
2024-09-04 21:32:38 -0500

    Studio part A

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,17 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+//import static java.lang.Math.PI;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is the radius of the circle?");
+        Double Radius = input.nextDouble();
+        double Area = 3.14 * Radius * Radius;
+
+        System.out.println("A circle with a radius of " + Radius + " has an area of " + Area);
+    }
+}
