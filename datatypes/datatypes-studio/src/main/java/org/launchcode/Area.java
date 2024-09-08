a94cd3c05cecb31ddcdc601dafc7e3a8ca6889d9
alexdavies440 <alexdavies440@gmail.com>
2024-09-07 21:21:36 -0500

    Bonus practice

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -13,6 +13,16 @@ public class Area {
         //double Area = 3.14 * Radius * Radius;
         Double Area = Circle.getArea(Radius);
 
-        System.out.println("A circle with a radius of " + Radius + " has an area of " + Area);
+        String negativeError = "Radius can't be negative!";
+
+        if (Radius < 0) {
+            for (char c : negativeError.toCharArray()) {
+                System.out.println(c);
+            }
+        } else {
+            System.out.println("A circle with a radius of " + Radius + " has an area of " + Area);
+        }
     }
+
 }
+
