82f401d09e4d46b1c39fcf83a62f0743075ee28a
alexdavies440 <alexdavies440@gmail.com>
2024-09-09 23:27:15 -0500

    formatting

--- control-flow-and-collections/studio/counting/characters/src/main/java/CountCharacters.java
+++ control-flow-and-collections/studio/counting/characters/src/main/java/CountCharacters.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class CountCharacters {
     public static void main(String[] args) {
         String str = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
-        HashMap<Character, Integer> characterWithCount= new HashMap<>();
+        HashMap<Character, Integer> characterWithCount = new HashMap<>();
         Character ch;
 
         ArrayList<Character> uniqueCharacters = new ArrayList<>();
@@ -29,20 +29,20 @@ public class CountCharacters {
             for (int j = 0; j < str.length(); j++) {
 
                 if (str.charAt(j) == uniqueCharacters.get(i)) {
-                    count ++;
+                    count++;
                 }
 
             }
             characterCount.add(count);
-            }
+        }
         //add to HashMap:
         for (int i = 0; i < uniqueCharacters.size(); i++) {
             characterWithCount.put(uniqueCharacters.get(i), characterCount.get(i));
 
         }
-        for (Map.Entry<Character, Integer> C: characterWithCount.entrySet()) {
+        for (Map.Entry<Character, Integer> C : characterWithCount.entrySet()) {
             System.out.println(C.getKey() + ": " + C.getValue());
         }
-        }
-
     }
+
+}
