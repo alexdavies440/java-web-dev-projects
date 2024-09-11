8c21a67c4233c9d66cb7db4fe58817b198fda6ae
alexdavies440 <alexdavies440@gmail.com>
2024-09-10 23:54:55 -0500

    update

--- control-flow-and-collections/studio/counting/characters/src/main/java/CountCharacters.java
+++ control-flow-and-collections/studio/counting/characters/src/main/java/CountCharacters.java
@@ -10,6 +10,7 @@ public class CountCharacters {
         String str = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
         HashMap<Character, Integer> characterWithCount = new HashMap<>();
         Character ch;
+        int[] arr = new int[3];
 
         ArrayList<Character> uniqueCharacters = new ArrayList<>();
         ArrayList<Integer> characterCount = new ArrayList<>();
