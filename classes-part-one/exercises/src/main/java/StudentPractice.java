128fb3f69ce06b0c4139b4c5a078dc8411038e5a
alexdavies440 <alexdavies440@gmail.com>
2024-09-11 22:57:27 -0500

    some practice

--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
+
 public class StudentPractice {
     public static void main(String[] args){
         //instantiate your Student class below
 
 Student alex = new Student("Alex", 7777, 1, 4.0);
-Teacher jay = new Teacher("Shawn", "IA", "Unit 2", 500);
+ArrayList studentAlex = new ArrayList();
+studentAlex.add(alex.getName());
+studentAlex.add(alex.getStudentId());
+studentAlex.add(alex.getNumberOfCredits());
+studentAlex.add(alex.getGpa());
+Teacher shawn = new Teacher("Shawn", "IA", "Unit 2", 500);
+Course launchcode = new Course("LaunchCode", shawn, studentAlex);
+for (int i = 0; i < studentAlex.size(); i++) {
+    System.out.println(studentAlex.get(i));
+
+}
     }
 }
