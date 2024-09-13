c81113c426e6d50278ab2ed4c0ac02973a2b6e1b
alexdavies440 <alexdavies440@gmail.com>
2024-09-12 22:30:09 -0500

    Added some menuItems

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
@@ -1,9 +1,19 @@
 package org.launchcode;
+import java.util.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        MenuItem bread = new MenuItem(5.00, "Rye Bread smeared with butter", " Appetizer ");
+        MenuItem chickenWing = new MenuItem(82.75, "A bathtub full of spicy chicken wings, bathed in hot sauce and ranch", "Entree");
+        MenuItem ultimatePBJ = new MenuItem(4.50, "Choke on two inches of peanutbutter and grape jelly between two slices of sandwhich bread, comes with a pint of milk", "Dessert");
 
+
+
+        System.out.println(bread);
     }
 }
--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
@@ -1,4 +1,40 @@
 package org.launchcode;
 
+import java.util.ArrayList;
+
 public class MenuItem {
+
+private Double price;
+private String description;
+private String category;
+
+public MenuItem(Double price, String description, String category) {
+    this.price = price;
+    this.description = description;
+    this.category = category;
+}
+
+public void setPrice(Double price) {
+    this.price = price;
+}
+
+public void setDescription(String description) {
+    this.description = description;
+}
+
+public void setCategory(String category) {
+    this.category = category;
+}
+
+public Double getPrice() {
+    return price;
+}
+
+public String getDescription() {
+    return description;
+}
+
+public String getCategory() {
+    return category;
+}
 }
