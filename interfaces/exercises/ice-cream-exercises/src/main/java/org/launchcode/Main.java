feea096874714dfc47c59c0676f9c626e9573edd
alexdavies440 <alexdavies440@gmail.com>
2024-09-26 16:49:22 -0500

    Exercises complete, will do bonus later

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -17,8 +17,17 @@ public class Main {
         for (Flavor flavor : flavors) {
             System.out.println(flavor.getName());
         }
+        System.out.println();
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
-
+        System.out.println("Unsorted");
+        for (Cone cone : cones) {
+            System.out.println(cone.getCost());
+        }
+        cones.sort(new ConeComparator());
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        System.out.println("Sorted");
+        for (Cone cone : cones) {
+            System.out.println(cone.getCost());
+        }
     }
 }
