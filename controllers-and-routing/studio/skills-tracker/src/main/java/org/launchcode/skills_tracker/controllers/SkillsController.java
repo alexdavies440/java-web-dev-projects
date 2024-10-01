924f6b21161b8949a6b1d02b295a9437a32a32b1
alexdavies440 <alexdavies440@gmail.com>
2024-09-30 23:12:03 -0500

    Studio done, may try to make button redirect to form later

--- controllers-and-routing/studio/skills-tracker/src/main/java/org/launchcode/skills_tracker/controllers/SkillsController.java
+++ controllers-and-routing/studio/skills-tracker/src/main/java/org/launchcode/skills_tracker/controllers/SkillsController.java
@@ -2,6 +2,7 @@ package org.launchcode.skills_tracker.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -19,9 +20,11 @@ public class SkillsController {
                 "<li>JavaScript</li>" +
                 "<li>React</li>" +
                 "</ol>" +
+                "<button type = 'submit' name='toForm'>Click Here</button>" +
                 "</body>" +
                 "</html>";
     }
+
     @GetMapping("/form")
     public String form() {
         return "<html>" +
@@ -33,7 +36,7 @@ public class SkillsController {
                 "<br>" +
                 "<label>My favourite language: </label>" +
                 "<br>" +
-                "<select>" +
+                "<select name='firstChoice'>" +
                 "<option value='java'>Java</option>" +
                 "<option value='javascript'>JavaScript</option>" +
                 "<option value='react'>React</option>" +
@@ -41,7 +44,7 @@ public class SkillsController {
                 "<br>" +
                 "<label>My second favourite language: </label>" +
                 "<br>" +
-                "<select>" +
+                "<select name='secondChoice'>" +
                 "<option value='java'>Java</option>" +
                 "<option value='javascript'>JavaScript</option>" +
                 "<option value='react'>React</option>" +
@@ -49,7 +52,7 @@ public class SkillsController {
                 "<br>" +
                 "<label>My third favourite language: </label>" +
                 "<br>" +
-                "<select>" +
+                "<select name='thirdChoice'>" +
                 "<option value='java'>Java</option>" +
                 "<option value='javascript'>JavaScript</option>" +
                 "<option value='react'>React</option>" +
@@ -58,6 +61,20 @@ public class SkillsController {
                 "<input type='submit' value='submit'/>" +
                 "</body>" +
                 "</html>";
+    }
+
+    @PostMapping("/form")
+    public String submitForm(@RequestParam String name, @RequestParam String firstChoice, @RequestParam String secondChoice, @RequestParam String thirdChoice) {
+        return "<html>" +
+                "<body style='color: chartreuse; background-color: black;'>" +
+                "<h1 style='text-shadow: 0 0 12px blue;'>" + name + "</h1>" +
+                "<table style='font-size: 2.5em; border: 3px solid; padding: 8px; text-shadow: 0 0 12px blue;'>" +
+                "<tr>" + "<td style='text-align: left;'>First choice: </td>" + "<td>" + firstChoice + "</td>" + "</tr>" +
+                "<tr>" + "<td style='text-align: left;'>Second choice: </td>" + "<td>" + secondChoice + "</td>" + "</tr>" +
+                "<tr>" + "<td style='text-align: left;'>Third choice: </td>" + "<td>" + thirdChoice + "</td>" + "</tr>" +                "</table>" +
+                "</body>" +
+                "</html>";
+
     }
 
 }
