7abf209e6c29da882dbe401e65afb3c4d036f29a
alexdavies440 <alexdavies440@gmail.com>
2024-10-03 00:14:57 -0500

    Exercises done

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -18,6 +18,10 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        for (String i : studentFiles.keySet()) {
+            System.out.println(i + ": " + CheckFileExtension(studentFiles.get(i)));
+        }
+
     }
 
     public static void Divide(int x, int y) {
@@ -30,11 +34,28 @@ public class Main {
             }
         }
         int result = x / y;
-        System.out.println(result);
+        System.out.println(x + "/" + y + " = " + result);
     }
 
-//    public static int CheckFileExtension(String fileName)
-//    {
-//        // Write code here!
-//    }
+    public static int CheckFileExtension(String fileName) {
+        int points = 0;
+
+        if (fileName.endsWith(".java")) {
+            points = 1;
+        }
+        else if (fileName.isEmpty()) {
+            try {
+                throw new Exception("File Name Empty!");
+            } catch(Exception e) {
+                e.getMessage();
+                e.printStackTrace();
+            }
+            points = -1;
+        }
+        else {
+            points = 0;
+        }
+
+        return points;
+    }
 }
