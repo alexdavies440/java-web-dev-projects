77079ba1483f01710c63e4e779ece20c5cc51ac3
alexdavies440 <alexdavies440@gmail.com>
2024-10-02 23:21:38 -0500

    First excercise done

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -1,10 +1,15 @@
 package org.launchcode;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.HashMap;
 
 public class Main {
+
     public static void main(String[] args) {
         // Test out your Divide() function!
+        Divide(10, 2);
+        Divide(10, 0); //Throws exception
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -15,13 +20,21 @@ public class Main {
         // Test out your CheckFileExtension() function!
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
+    public static void Divide(int x, int y) {
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("Cannot divide by zero (Silly goose!)");
+            } catch (ArithmeticException e) {
+                e.getStackTrace();
+                System.out.println(e.getMessage());
+            }
+        }
+        int result = x / y;
+        System.out.println(result);
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
-    }
+//    public static int CheckFileExtension(String fileName)
+//    {
+//        // Write code here!
+//    }
 }
