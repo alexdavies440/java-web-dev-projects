2d63c43753edaf9c26422df9a23688f44bb8a81e
alexdavies440 <alexdavies440@gmail.com>
2024-10-06 21:46:19 -0500

    updated redirect...

--- codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,15 +13,24 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
+
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("event1");
-        events.add("event2");
-        events.add("event3");
         model.addAttribute("events", events);
         return "events/index";
     }
+    //lives at /events/create
+@GetMapping("create")
+public String renderCreateEventForm() {
+        return "events/create";
+}
 
-
+@PostMapping("create")
+public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        // Redirects to root of this controller
+        return "redirect:/events";
+}
 }
