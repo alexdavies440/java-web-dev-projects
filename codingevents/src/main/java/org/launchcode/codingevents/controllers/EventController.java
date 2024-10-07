778f3b51e75a88e26b0cdcd7253b296d255fe2c6
alexdavies440 <alexdavies440@gmail.com>
2024-10-07 12:45:29 -0500

    First commit

--- codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,17 +8,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+
+    HashMap<String, String> events = new HashMap();
 
     @GetMapping
     public String displayEvents(Model model) {
         model.addAttribute("events", events);
+
+        events.put("Java Day", "Learn the entirety of Java in one day while drinking nothing but java");
+        events.put("The Great Escape", "Learn to use a state setter to call for help to get out of an escape room before the timer runs out!");
+        events.put("The Spanish Inquisition", "Practice coding interviews");
         return "events/index";
     }
     //lives at /events/create
@@ -29,8 +35,10 @@ public String renderCreateEventForm() {
 
 @PostMapping("create")
 public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
-        // Redirects to root of this controller
+
+
+
+    // Redirects to root of this controller
         return "redirect:/events";
 }
 }
