3cb1a7a5247bfc3e8d9d321dfed858d99e8266ad
alexdavies440 <alexdavies440@gmail.com>
2024-10-18 22:26:50 -0500

    Made month enum to allow sorting by birthday later

---
+++ contacts-project/src/main/java/org/launchcode/Month.java
@@ -0,0 +1,25 @@
+package org.launchcode;
+
+public enum Month {
+
+    JANUARY("January", 1),
+    FEBRUARY("February", 2),
+    MARCH("March", 3),
+    APRIL("April", 4),
+    MAY("May", 5),
+    JUNE("June", 6),
+    JULY("July", 7),
+    AUGUST("August", 8),
+    SEPTEMBER("September", 9),
+    OCTOBER("October", 10),
+    NOVEMBER("November", 11),
+    DECEMBER("December", 12);
+
+    private final String displayMonth;
+    private final int monthOrder;
+
+    public Month(String displayMonth, int monthOrder) {
+        this.displayMonth = displayMonth;
+        this.monthOrder = monthOrder;
+    }
+}
